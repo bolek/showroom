035d846a7067defe2dd016d6c48d08cc46c76179
Bolek Kurowski <bolek.kurowski@gmail.com>
2021-03-24 21:45:53 -0400

    Layout with dummy data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>
+        <img className='user-avatar' src='placeholder.png' alt='avatar' />
+        <span>John Doe</span>
+      </h1>
+
+      <dl className='meta'>
+        <dt>Followers</dt>
+        <dd>100</dd>
+        <dt>Public repos</dt>
+        <dd>50</dd>
+      </dl>
+
+      <h2>Repositories</h2>
+      <div className='repository'>
+        <h3>Rails</h3>
+        <p>Project description - Ruby on Rails</p>
+
+        <dl className='meta'>
+          <dt>Topics</dt>
+          <dd>ruby, rails, html</dd>
+          <dt>Watchers</dt>
+          <dd>100</dd>
+          <dt>Stargazers</dt>
+          <dd>8</dd>
+        </dl>
+      </div>
+      <hr />
+      <div className='repository'>
+        <h3>sprockets</h3>
+        <p>some repository description</p>
+
+        <dl className='meta'>
+          <dt>Topics</dt>
+          <dd>ruby, rails, html</dd>
+          <dt>Watchers</dt>
+          <dd>100</dd>
+          <dt>Stargazers</dt>
+          <dd>8</dd>
+        </dl>
+      </div>
     </div>
   );
 }
